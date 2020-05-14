5e91440f8b6ea2982d27a9c80ceeb669de89d228
Ali-Sharifi-Neyestani <sh4rifi@gmail.com>
2020-05-15 02:53:55 +0430

    config webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,30 @@
 const mix = require('laravel-mix');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+
+mix.webpackConfig({
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: 'resources/assets/images',
+                to: 'assets/images', // Laravel mix will place this in 'public/img'
+            },
+            {
+                from: 'resources/uploads',
+                to: 'uploads', // Laravel mix will place this in 'public/img'
+            }
+        ]),
+        new ImageminPlugin({
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            plugins: [
+                imageminMozjpeg({
+                    quality: 80,
+                })
+            ]
+        })
+    ]
+});
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +37,28 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/assets/js')
+    .sass('resources/sass/app.scss', 'public/assets/css')
+    .styles([
+        'resources/assets/css/icons.min.css',
+        'resources/assets/css/bootstrap.min.css',
+        'resources/assets/css/plugins.min.css',
+        'resources/assets/css/colors.css',
+        'resources/assets/css/styles.css',
+    ], 'public/assets/css/app.css');
+;
+
+
+mix.scripts([
+    'public/assets/js/app.js',
+    'resources/assets/js/jquery.min.j',
+    'resources/assets/js/bootstrap.min.j',
+    'resources/assets/js/plugins.min.j',
+    'resources/assets/js/main-scripts.j',
+], 'public/assets/js/app.js');
+
+
+mix.copy('resources/assets/fonts/', 'public/assets/fonts');
+
+
+
