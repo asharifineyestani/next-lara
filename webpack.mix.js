16d384c3e131faf0cf7c0b62641c47b3cbb55009
Ali-Sharifi-Neyestani <sh4rifi@gmail.com>
2020-05-15 03:01:54 +0430

    add assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,10 +10,10 @@ mix.webpackConfig({
                 from: 'resources/assets/images',
                 to: 'assets/images', // Laravel mix will place this in 'public/img'
             },
-            {
-                from: 'resources/uploads',
-                to: 'uploads', // Laravel mix will place this in 'public/img'
-            }
+            // {
+            //     from: 'resources/uploads',
+            //     to: 'uploads', // Laravel mix will place this in 'public/img'
+            // }
         ]),
         new ImageminPlugin({
             test: /\.(jpe?g|png|gif|svg)$/i,
@@ -39,23 +39,24 @@ mix.webpackConfig({
 
 mix.js('resources/js/app.js', 'public/assets/js')
     .sass('resources/sass/app.scss', 'public/assets/css')
-    .styles([
-        'resources/assets/css/icons.min.css',
-        'resources/assets/css/bootstrap.min.css',
-        'resources/assets/css/plugins.min.css',
-        'resources/assets/css/colors.css',
-        'resources/assets/css/styles.css',
-    ], 'public/assets/css/app.css');
+    .sass('resources/assets/scss/style.scss', 'public/assets/css')
+    // .styles([
+    //     'resources/assets/css/icons.min.css',
+    //     'resources/assets/css/bootstrap.min.css',
+    //     'resources/assets/css/plugins.min.css',
+    //     'resources/assets/css/colors.css',
+    //     'resources/assets/css/styles.css',
+    // ], 'public/assets/css/app.css');
 ;
 
 
-mix.scripts([
-    'public/assets/js/app.js',
-    'resources/assets/js/jquery.min.j',
-    'resources/assets/js/bootstrap.min.j',
-    'resources/assets/js/plugins.min.j',
-    'resources/assets/js/main-scripts.j',
-], 'public/assets/js/app.js');
+// mix.scripts([
+//     'public/assets/js/app.js',
+//     'resources/assets/js/jquery.min.j',
+//     'resources/assets/js/bootstrap.min.j',
+//     'resources/assets/js/plugins.min.j',
+//     'resources/assets/js/main-scripts.j',
+// ], 'public/assets/js/app.js');
 
 
 mix.copy('resources/assets/fonts/', 'public/assets/fonts');
