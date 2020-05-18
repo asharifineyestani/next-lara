202a47228e0ca08cb4a172795c9c2f6edb78d960
Ali-Sharifi-Neyestani <sh4rifi@gmail.com>
2020-05-18 23:16:44 +0430

    add heading

---
+++ resources/views/components/heading.blade.php
@@ -0,0 +1,21 @@
+<header class="el-heading {{$items['position'] ?? null}} {{$items['className'] ?? null }}">
+
+    <h1>{{$items['h1'] ?? null }}</h1>
+
+    @isset($items['subTitle'])
+        <p class="subtitle">{{$items['subTitle'] ?? null}}</p>
+    @endif
+
+    @isset($items['title'])
+        <h2>
+            @isset($items['subTitle'])
+                <span class="display-block fw-100">{{$items['label'] }}</span>
+            @endisset
+            {{$items['title']}}
+        </h2>
+    @endisset
+
+    @isset($items['description'])
+        <p>{{$items['description']}}</p>
+    @endisset
+</header>
