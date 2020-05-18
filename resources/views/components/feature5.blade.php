f08562096e2249e976de82ddb29380f3bc51e790
Ali-Sharifi-Neyestani <sh4rifi@gmail.com>
2020-05-19 00:23:25 +0430

    add web development

---
+++ resources/views/components/feature5.blade.php
@@ -0,0 +1,20 @@
+<section class="section section-features section-features-1">
+    <div class="display-spacing">
+        <div class="container">
+            <x-section :items="$heading" view="heading"/>
+            <div class="row mb--30">
+                @foreach($items as $item)
+                    <div class="col-xl-2 col-md-2 col-sm-4">
+                        <div class="feature-item">
+                            <div class="image">
+                                <img src={{$item['image']}} alt="alt"/>
+                            </div>
+                            <h3>{{$item['title']}}</h3>
+
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/pages/web_development.blade.php
+++ resources/views/pages/web_development.blade.php
@@ -3,10 +3,8 @@
 
     <x-header/>
     <x-section :items="$hero" :heading="$hero['heading']" view="hero3" />
-    <x-section :items="$feature['items']" :heading="$feature['heading']" view="feature2" />
     <x-section :items="$story['items']" :heading="$story['heading']" view="story"/>
-    <x-section :items="$feature2['items']" :heading="$feature2['heading']" view="feature3" />
-    <x-section :items="$manager['items']" :heading="$manager['heading']" view="manager"/>
+    <x-section :items="$feature2['items']" :heading="$feature2['heading']" view="feature5" />
     <x-section :items="$call['items']" :heading="$call['heading']" view="call_to_action"/>
     <x-footer/>
 
